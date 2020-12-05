2f29c80ae363d62b52496b58696edd141fb58bb1
Michiya Nishida <m.nishida@sint.co.jp>
2020-12-05 08:10:00 +0000

    Merged PR 22: github workflowを追加
    github workflowを追加

--- client/client/src/components/typeManager.ts
+++
@@ -1,9 +0,0 @@
-export type RadioButtonValue = string | number;
-
-export type StepStatus =
-  | 'wait'
-  | 'process'
-  | 'finish'
-  | 'error'
-  | 'success'
-  | undefined;
--- client/client/src/layouts/playing.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nuxt />
-</template>
-<script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
-
-@Component
-export default class extends Vue {}
-</script>
